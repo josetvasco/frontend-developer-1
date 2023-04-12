0e953b1a0374829fef8538effcc924d86fe2fb66
josetvasco <torovascojosedaniel@gmail.com>
2023-04-11 21:09:43 -0500

    Agregado la interactividad del carrito de compras

--- main.js
+++ main.js
@@ -2,15 +2,39 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.menu-mobile');
 const burgerMenu = document.querySelector('.menu');
+const cartDetail = document.querySelector('.product-detail');
+const cartIcon = document.querySelector('.navbar-shopping-cart');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive')
+    desktopMenu.classList.toggle('inactive');
+
+    if (cartDetail.classList.contains('activo-shopping-cart')) {
+        cartDetail.classList.remove('activo-shopping-cart');
+    }
 }
 
 burgerMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('activo')
+    mobileMenu.classList.toggle('activo-menu-mobile');
+
+    if (cartDetail.classList.contains('activo-shopping-cart')) {
+        cartDetail.classList.remove('activo-shopping-cart');
+    }
+}
+
+cartIcon.addEventListener('click', toggleShoppingCart);
+
+function toggleShoppingCart() {
+    cartDetail.classList.toggle('activo-shopping-cart');
+
+    if (mobileMenu.classList.contains('activo-menu-mobile')) {
+        mobileMenu.classList.remove('activo-menu-mobile');
+    }
+
+    if (!desktopMenu.classList.contains('inactive')) {
+        desktopMenu.classList.add('inactive')
+    }
 }
